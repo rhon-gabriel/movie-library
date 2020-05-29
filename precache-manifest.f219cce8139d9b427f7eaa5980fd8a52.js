fd1179f0fb9fb397d0871971f9d4bf686306cdf1
Rhon <rhoneil@woshapp.se>
2020-05-29 09:44:47 +0200

    Updates

---
+++ precache-manifest.f219cce8139d9b427f7eaa5980fd8a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7224b44bbcb722154ca5fcc66c89d5a7",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "9b44d8c11e2215509d6a",
+    "url": "/movie-library/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2876434ab159dbd27996",
+    "url": "/movie-library/static/js/2.2eff05c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-library/static/js/2.2eff05c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b44d8c11e2215509d6a",
+    "url": "/movie-library/static/js/main.953f8bb0.chunk.js"
+  },
+  {
+    "revision": "9724b36f733357f95888",
+    "url": "/movie-library/static/js/runtime-main.f40346a7.js"
+  }
+]);
